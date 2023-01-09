1637b7bf65704d712b2c880e9288f04ffb432d25
t0pdog <topillustrated@gmail.com>
2023-01-09 20:16:17 +0200

    CI / CD

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY", default="***")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", default=False)
+# DEBUG = os.getenv("DEBUG", default=False)
+DEBUG = True
 
 CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOWED_ORIGINS = [
